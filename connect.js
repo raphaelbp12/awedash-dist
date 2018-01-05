65ad87ec8e0af027a05242f0f8038ffbdffcaeeb
Raphael Parreira <raphaelbp12@gmail.com>
2018-01-05 18:18:51 -0200

    2

---
+++ connect.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+ app = express(),
+ http = require('http'),
+ httpServer = http.Server(app);
+
+app.use('/static', express.static('static'));
+
+app.get('/*', function(req, res) {
+console.log('request');
+ res.sendFile(__dirname + '/index.html');
+});
+app.listen(80);
